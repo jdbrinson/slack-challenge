7e841c239d6979cbfb4a761bb107d8bc9f2c8369
Julien Brinson <jbrinson@stanford.edu>
2016-08-18 14:04:13 -0400

    Initial Commit

---
+++ Slack code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Slack code
+//
+//  Created by Julien Brinson on 8/18/16.
+//  Copyright © 2016 Julien Brinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
