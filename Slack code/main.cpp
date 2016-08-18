0337e937c184a656f7df1ecbc24842c1f9daa9e4
Julien Brinson <jbrinson@stanford.edu>
2016-08-18 14:10:08 -0400

    Initial commit of the project. Included pseudo code of the coming code implementation

--- Slack code/main.cpp
+++ Slack code/main.cpp
@@ -8,8 +8,15 @@
 
 #include <iostream>
 
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    /*Pseudo code to implement:
+     1)intro output
+     2)get/list all available channels
+     3)wait for select channel/show message history
+     4)cache data
+     */
     std::cout << "Hello, World!\n";
     return 0;
 }
