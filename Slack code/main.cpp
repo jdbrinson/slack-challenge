fe27ff8d223fdc48e9dd019b5550891a846e0068
Julien Brinson <jbrinson@stanford.edu>
2016-08-19 01:17:41 -0400

    Current Functionality: prints channel list from provided token

--- Slack code/main.cpp
+++ Slack code/main.cpp
@@ -7,16 +7,87 @@
 //
 
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include "json/json.h"
 
+static std::string TOKEN = "xoxp-4698769766-4698769768-18910479235-8fa82d53b2";
+static std::string APIURL = "https://slack.com/api/";
+
+size_t retrieve_data_callback(char *data, size_t size, size_t nmemb, void *raw_json){
+    ((std::string *)raw_json)->append(data, size*nmemb);
+    return size*nmemb;
+}
+
+void call_slack(std::string slack_token, std::string methodName, std::string &payload, std::string param){
+    CURL *curl;
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, retrieve_data_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &payload);
+    std::string request_url = APIURL + methodName +"?token=" + slack_token + param;
+    curl_easy_setopt(curl, CURLOPT_URL, request_url.c_str());
+    if(curl){
+        curl_easy_perform(curl);
+    }
+    
+    curl_easy_cleanup(curl);
+}
+
+std::vector<std::string> extract_channels(std::string &payload){
+    Json::Value jObj;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse(payload.c_str(), jObj);
+    if(!parsingSuccessful){
+        std::cout<< "failed to parse" << reader.getFormatedErrorMessages();
+    }
+    Json::Value j_channelList = jObj.get("channels", "default");
+    std::vector<std::string> channels;
+    int channel_index = 0;
+    const char* channel_name = "name";
+    while(j_channelList.isValidIndex(channel_index)){
+        channels.push_back(j_channelList[channel_index][channel_name].asString());
+        channel_index++;
+    }
+    return channels;
+//    std::vector<std::string> channels;
+//    return channels;
+}
+
+void get_channel_list(std::string slack_token){
+    std::string payload = "";
+    std::string methodName = "channels.list";
+    std::string param = "";
+    call_slack(slack_token, methodName, payload, param);
+    std::vector<std::string> channels = extract_channels(payload);
+    std::cout << "Channel List:";
+    for(std::vector<std::string>::iterator it = channels.begin(); it != channels.end(); ++it){
+        std::cout << "\n" << *it;
+    }
+    std::cout << "\n";
+}
+
+
+void get_channelList( std::string &payload){
+    //extract_channel_list(payload);
+    //print channel list
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     /*Pseudo code to implement:
      1)intro output
-     2)get/list all available channels
-     3)wait for select channel/show message history
+     */
+    std::string slack_token;
+    std::cout << "Welcome to Slack!\n";
+    std::cout << "Please provide token: ";
+    
+    std::cin >> slack_token;
+
+     //2)get/list all available channels
+    get_channel_list(slack_token);
+     /*3)wait for select channel/show message history
      4)cache data
      */
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
